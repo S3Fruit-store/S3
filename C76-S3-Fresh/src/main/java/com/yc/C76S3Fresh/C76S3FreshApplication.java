f3a1bab405ca098b4bb50d1ecc50ea60b2d9f429
6606 <lhdck>
2020-04-01 16:06:20 +0800

    初始化工程

---
+++ C76-S3-Fresh/src/main/java/com/yc/C76S3Fresh/C76S3FreshApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C76S3Fresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C76S3FreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C76S3FreshApplication.class, args);
+	}
+
+}
---
+++ C76-S3-Fresh/src/test/java/com/yc/C76S3Fresh/C76S3FreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C76S3Fresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C76S3FreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
