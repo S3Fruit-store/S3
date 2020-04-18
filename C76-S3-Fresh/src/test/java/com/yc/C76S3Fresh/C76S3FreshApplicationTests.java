6a6d4faee1cd0a559614da1d7e933d40ed77beae
lenovo <lenovo@LAPTOP-S8EBJKDA>
2020-04-18 09:30:12 +0800

    11

--- C76-S3-Fresh/src/test/java/com/yc/C76S3Fresh/C76S3FreshApplicationTests.java
+++ C76-S3-Fresh/src/test/java/com/yc/C76S3Fresh/C76S3FreshApplicationTests.java
@@ -1,21 +1,48 @@
 package com.yc.C76S3Fresh;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.yc.fresh.C76S3FreshApplication;
+import com.yc.fresh.bean.Product;
+import com.yc.fresh.bean.ProductExample;
+import com.yc.fresh.bean.ProductExample.Criteria;
+import com.yc.fresh.dao.ProductMapper;
 import com.yc.fresh.dao.UserMapper;
 
-@SpringBootTest
+@SpringBootTest(classes=C76S3FreshApplication.class)
 class C76S3FreshApplicationTests {
 	
+
+	@Resource
+	private ProductMapper pm;
 	@Resource
 	private UserMapper um;
+	
 	@Test 
 	void contextLoads() {
 		Assert.isTrue(um.selectByExample(null).size() > 0, "结果集数量不正确!");
 	}
 
+	@Test
+	void HotProduct(){
+		Assert.isTrue(pm.selectByExample(null).size() > 0, "结果集数量不正确!");
+		
+		ProductExample pe =new ProductExample();
+		pe.setOrderByClause("fnumber DESC");//降序排列
+		
+		Page<Product> page=PageHelper.startPage(1, 6);
+		pm.selectByExample(pe);
+		
+		for (Product product : page) {
+			System.out.println(product.getFname());
+		}
+	}
 }
